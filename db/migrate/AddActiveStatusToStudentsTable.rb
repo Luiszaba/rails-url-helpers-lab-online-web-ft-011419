b31691126ce37346f5ee60ec419ef9d0b6997c24
Luis Zabala <luis.zabala076@gmail.com>
2019-03-22 18:56:10 +0000

    Automatically backed up by Learn

--- db/migrate/AddActiveStatusToStudentsTable.rb
+++ db/migrate/AddActiveStatusToStudentsTable.rb
@@ -1,4 +1,4 @@
-class AddActiveStatusToStudentsTable < ActiveRecord::Migration[5.1]
+class AddActiveStatus < ActiveRecord::Migration[5.1]
 
 def change
   add_column :students, :active, :boolean, default: false
